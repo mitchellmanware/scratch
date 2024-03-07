008534f8e771f5768e6a86823e6efbc018b3a99e
mitchellmanware <memanware@gmail.com>
2024-03-07 08:55:59 -0500

    edit functions

--- amadeus/rtorch/tensor_explore.R
+++ amadeus/rtorch/tensor_explore.R
@@ -2,19 +2,45 @@
 # March 06, 2024
 
 # import covariate data
-hms <- readRDS("data/hms.RDS")
-pop <- readRDS("data/pop.RDS")
+source("../../beethoven/covariates_data_frame.R")
+d <- covariates_data_frame(
+  covariates = list.files("data/", full.names = TRUE),
+  type = "rds",
+  dates = c("2020-12-01", "2021-01-31"),
+  sample = 10
+)
+# summary
+summary(d)
+head(d)
+tail(d)
+# check for NAs
+all(!is.na(d))
 
-# sample
-s <- sample(hms$site_id, size = 10, replace = FALSE)
-h <- hms[which(hms$site_id %in% s), ]
-p <- pop[which(pop$site_id %in% s), ]
+# simulate PM outcome data
+d$pm2.5 <- rgamma(10 * 62, 32, 1.6)
 
-# same site locations
-all(unique(h$site_id) == unique(p$site_id))
-summary(h)
-summary(p)
+# libraries
+library(torch)
+library(luz)
+library(data.table)
+devtools::load_all("/Volumes/SET/Projects/NRT-AP-Model")
 
+# covariate tensor
+xcol <- which(!(colnames(d) %in% c("site_id", "time", "pm2.5")))
+xtensor <- 
+  d[, xcol] |>
+  # data.table::melt(measure.vars = colnames(d)[xcol]) |>
+  # _[, 2] |>
+  as.matrix() |>
+  torch::torch_tensor(dtype = torch::torch_float()) |>
+  torch::torch_reshape(shape = list(1, 10, 62, 4))
+xtensor
 
-
-
+# response tensor
+ycol <- which(colnames(d) == "pm2.5")
+ytensor <-
+  d[,ycol] |>
+  as.matrix() |>
+  torch::torch_tensor(dtype = torch::torch_float()) |>
+  torch::torch_reshape(shape = list(1, 10, 62, 1))
+ytensor
--- beethoven/covariates_data_frame.R
+++ beethoven/covariates_data_frame.R
@@ -1,11 +1,11 @@
 # function to create single data frame for all covariates
 
 covariates_data_frame <-
-  function(
-      covariates,
-      type = "rds",
-      id = "site_id",
-      dates = c("2018-01-01", "2022-01-01")) {
+  function(covariates,
+           type = "rds",
+           id = "site_id",
+           dates = c("2018-01-01", "2022-01-01"),
+           sample = NULL) {
     # define dates of interest
     dates_of_interest <- seq(
       as.Date(dates[1]),
@@ -14,11 +14,11 @@ covariates_data_frame <-
     )
     for (c in seq_along(covariates)) {
       # import data from rds files
-      if (type == "rds") {
+      if (type %in% c("rds", "RDS", ".rds", ".RDS")) {
         data <- readRDS(covariates[c])
         # import data from R objects
       } else {
-        data <- covariates[c]
+        data <- covariates[[c]]
       }
       # process time dimension of data
       data_time <- covariates_time(
@@ -32,7 +32,7 @@ covariates_data_frame <-
       ]
       # create merge identifier based on site and date
       data_time_filter$merge <- paste0(
-        data_time_filter[,id],
+        data_time_filter[, id],
         data_time_filter$time
       )
       if (c == 1) {
@@ -52,46 +52,57 @@ covariates_data_frame <-
       }
     }
     # retain only one identifier column
-    drop_id <- c("merge", "lon", "lat", "geom")
+    drop_id <- c("merge", "lon", "lat", "geom", "year", "date")
     covariates_df_1 <-
-     covariates_df[, !(colnames(covariates_df) %in% drop_id)]
-    return(covariates_df_1)
+      covariates_df[, !(colnames(covariates_df) %in% drop_id)]
+    # sample sites
+    if (is.null(sample)) {
+      return(data.frame(covariates_df_1))
+    } else if (!is.null(sample)) {
+      sample_locs <- sample(
+        covariates_df_1[,id],
+        size = sample,
+        replace = FALSE)
+      covariates_sample <- covariates_df_1[
+        covariates_df_1[,id] %in% sample_locs,
+      ]
+      return(data.frame(covariates_sample))
+    }
   }
 
 covariates_time <-
-  function(
-      data,
-      id = "site_id",
-      dates) {
+  function(data,
+           id = "site_id",
+           dates) {
     n <- colnames(data)
     if (!(id %in% n)) {
       stop(paste0("Identifier not detected in column name.\n"))
     }
     if ("time" %in% n) {
       cat(paste0("'time' defined data detected...\n"))
-      return(data)
+      return(data.frame(data))
     }
     if ("year" %in% n) {
       cat(paste0("'year' defined data detected...\n"))
-      years <- as.integer(data$year)
+      years <- as.integer(unique(data$year))
       data_dates <- NULL
       for (y in seq_along(years)) {
         data_year <- data[data$year == years[y]]
         dates_year <- seq(
-          as.Date(paste0(years[y], "01-01")),
-          as.Date(paste0(years[y], "12-31")),
+          as.Date(paste0(years[y], "-01-01")),
+          as.Date(paste0(years[y], "-12-31")),
           by = 1
         )
         data_merge <- merge(dates_year, data_year)
         colnames(data_merge)[1] <- "time"
         data_dates <- rbind(data_dates, data_merge)
       }
-      return(data_dates)
+      return(data.frame(data_dates))
     }
     if (all((c("time", "year") %in% n) == c(FALSE, FALSE))) {
       cat(paste0("Temporally undefined data detected...\n"))
       data_dates <- merge(dates, data)
       colnames(data_dates)[1] <- "time"
-      return(data_dates)
+      return(data.frame(data_dates))
     }
   }
